3930caaefb752ca159070e713749d9ac8cc23560
harryqin99 <harryqin1999@gmail.com>
2021-08-08 22:55:21 +0800

    Initial Commit

---
+++ src/main/java/tech/crn/server/crnproject/CrnProjectApplication.java
@@ -0,0 +1,13 @@
+package tech.crn.server.crnproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrnProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrnProjectApplication.class, args);
+	}
+
+}
