29a5d8e58fcfd2176409ba26c8c83054e481d213
harryqin99 <harryqin1999@gmail.com>
2021-08-08 22:55:21 +0800

    Initial Commit
    Initial Commit

---
+++ src/test/java/tech/crm/crmserver/CrmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.crm.crmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
