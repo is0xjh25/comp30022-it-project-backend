ca14927d5f5f4e8722faf0afdef3a89325e08613
harryqin99 <harryqin1999@gmail.com>
2021-08-08 22:55:21 +0800

    Initial Commit
    Initial Commit
    
    Initial Commit
    
    Initial Commit
    
    Initial Commit
    
    Initial Commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# comp30022-it-project-backend
-
-Hello World!
-
-To be continued...
---
+++ src/main/java/tech/crm/crmserver/CrmServerApplication.java
@@ -0,0 +1,13 @@
+package tech.crm.crmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmServerApplication.class, args);
+	}
+
+}
