9f1439142026ce2bee13d6a38e66e1799b14bea4
Lingxiao108105 <71265122+Lingxiao108105@users.noreply.github.com>
2021-09-05 17:48:27 +1000

    Update CrossOriginConfig.java

--- src/main/java/tech/crm/crmserver/config/CrossOriginConfig.java
+++ src/main/java/tech/crm/crmserver/config/CrossOriginConfig.java
@@ -11,7 +11,12 @@ public class CrossOriginConfig implements WebMvcConfigurer{
     static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE" };
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods(ORIGINS).maxAge(3600);
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080","https://comp30022-yyds.herokuapp.com/")
+                .allowCredentials(true)
+                .allowedMethods(ORIGINS)
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 
 }
