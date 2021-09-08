c77be370d3ed2fcfe28e629245a065f40366d144
Lingxiao108105 <71265122+Lingxiao108105@users.noreply.github.com>
2021-09-08 10:41:35 +1000

    search contacts by some details

--- src/main/java/tech/crm/crmserver/controller/ContactController.java
+++ src/main/java/tech/crm/crmserver/controller/ContactController.java
@@ -1,15 +1,16 @@
 package tech.crm.crmserver.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import tech.crm.crmserver.common.response.ResponseResult;
 import tech.crm.crmserver.dao.Contact;
-import tech.crm.crmserver.dao.Organization;
 import tech.crm.crmserver.service.ContactService;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -33,6 +34,27 @@ public class ContactController {
     public ResponseResult<Object> getContactById(@RequestParam("contact_id") Integer contactId) {
         Contact contact = contactService.getById(contactId);
         return ResponseResult.suc("success", contact);
+    }
+
+    /**
+     * search contacts by some details
+     * @param map
+     * @return
+     */
+    @GetMapping("/search")
+    public ResponseResult<Object> searchContact(@RequestBody Map<String,String> map) {
+        QueryWrapper<Contact> wrapper = new QueryWrapper<>();
+        for(Map.Entry<String,String> entry : map.entrySet()){
+            if(entry.getValue() == null){
+                continue;
+            }
+            wrapper.like(entry.getKey(),entry.getValue());
+        }
+        List<Contact> contacts = contactService.list(wrapper);
+        if(contacts.isEmpty()){
+            return ResponseResult.fail("no match result is found");
+        }
+        return ResponseResult.suc("success",contacts);
     }
 
 
