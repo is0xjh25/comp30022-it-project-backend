5ffb181182d81c2f92995e67bdc54ae420a6e44b
harryqin99 <harryqin1999@gmail.com>
2021-09-21 00:01:21 +0800

    Add updateContact/DeleteContact method

--- src/main/java/tech/crm/crmserver/controller/ContactController.java
+++ src/main/java/tech/crm/crmserver/controller/ContactController.java
@@ -2,12 +2,20 @@ package tech.crm.crmserver.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import tech.crm.crmserver.common.enums.PermissionLevel;
 import tech.crm.crmserver.common.response.ResponseResult;
 import tech.crm.crmserver.dao.Contact;
+import tech.crm.crmserver.dao.Department;
+import tech.crm.crmserver.dao.Permission;
+import tech.crm.crmserver.dto.ContactDTO;
 import tech.crm.crmserver.service.ContactService;
+import tech.crm.crmserver.service.DepartmentService;
+import tech.crm.crmserver.service.PermissionService;
+import tech.crm.crmserver.service.UserService;
 
 import java.util.HashMap;
 import java.util.List;
@@ -28,6 +36,15 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @Autowired
+    private PermissionService permissionService;
+
     /**
      * Search a contact's details
      * */
@@ -35,6 +52,118 @@ public class ContactController {
     public ResponseResult<Object> getContactById(@RequestParam("contact_id") Integer contactId) {
         Contact contact = contactService.getById(contactId);
         return ResponseResult.suc("success", contact);
+    }
+
+    @GetMapping()
+    public ResponseResult<Object> getContactByOrganizationIdAndDepartmentId(@RequestParam("organization_id") Integer organizationId,
+                                                                            @RequestParam("department_id") Integer departmentId,
+                                                                            @RequestParam("size") Integer size,
+                                                                            @RequestParam("current") Integer current) {
+        Integer userId = userService.getId();
+        if (organizationId == null) {
+            return ResponseResult.fail("missing organizationId");
+        }
+
+        Page<Contact> contacts = contactService.getContactByOrgIdAndDepartmentId(new Page<>(current, size), organizationId, departmentId, userId);
+
+        if (contacts.getTotal() == 0) {
+            return ResponseResult.suc("No contacts");
+        }
+        return ResponseResult.suc("success", contacts);
+    }
+
+    @PostMapping()
+    public ResponseResult<Object> createNewCustomer(@RequestBody ContactDTO contactDTO) {
+        // read in the customer details
+        Contact newContact = contactService.fromContactDTO(contactDTO);
+        // 1. check if departmentID is valid
+        // 2. check if the user has permission to insert a new customer
+        // 3. after all checkout, use ContactService to insert
+
+        Integer departmentId = newContact.getDepartmentId();
+        Department department = departmentService.getById(departmentId);
+        Integer departmentAddTo = department.getId();
+        if (department == null) {
+            return ResponseResult.fail("Department does not exist");
+        }
+
+        // check the user's permission level
+        Integer userID = userService.getId();
+        Permission myPermission = permissionService.findPermission(departmentAddTo, userID);
+        if (myPermission != null && myPermission.getAuthorityLevel().getLevel() < PermissionLevel.UPDATE.getLevel()) {
+            return ResponseResult.fail("Do not have authority to add new contact");
+        }
+
+        // check if the same contact already exists
+        List<Contact> contacts = contactService.getContactBasedOnSomeConditionFromDB(null,newContact.getEmail(), null, null, null, null, null, null, null);
+        if (contacts.size() > 0) {
+            return ResponseResult.fail("Contact with same email already exist!");
+        }
+        boolean addSucc = false;
+        try {
+            addSucc = contactService.save(newContact);
+        } catch (Exception e) {
+            return ResponseResult.fail("Adding a new contact fail");
+        }
+        if (!addSucc) {
+            return ResponseResult.fail("Adding a new contact fail");
+        }
+        return ResponseResult.suc("Add a new contact success");
+    }
+
+    @PutMapping
+    public ResponseResult<Object> updateContact(@RequestBody ContactDTO contactDTO) {
+        // For updating, it must have id
+        Contact newContact = contactService.fromContactDTO(contactDTO);
+        if (newContact.getId() == null) {
+            return ResponseResult.fail("Missing id of the contact to be updated");
+        }
+        Integer departmentId = newContact.getDepartmentId();
+        Department department = departmentService.getById(departmentId);
+        Integer departmentAddTo = department.getId();
+        if (department == null) {
+            return ResponseResult.fail("Department does not exist");
+        }
+
+        // check the user's permission level
+        Integer userID = userService.getId();
+        Permission myPermission = permissionService.findPermission(departmentAddTo, userID);
+        if (myPermission != null && myPermission.getAuthorityLevel().getLevel() < PermissionLevel.UPDATE.getLevel()) {
+            return ResponseResult.fail("Do not have authority to update new contact");
+        }
+
+        if (contactService.updateContact(newContact)) {
+            return ResponseResult.suc("update a new contact success");
+        }
+        return ResponseResult.fail("update a new contact fail");
+    }
+
+    @DeleteMapping
+    public ResponseResult<Object> deleteContact(@RequestBody ContactDTO contactDTO) {
+
+        Contact newContact = contactService.fromContactDTO(contactDTO);
+        if (newContact.getId() == null) {
+            return ResponseResult.fail("Missing id of the contact to be updated");
+        }
+
+        Integer departmentId = newContact.getDepartmentId();
+        Department department = departmentService.getById(departmentId);
+        Integer departmentAddTo = department.getId();
+        if (department == null) {
+            return ResponseResult.fail("Department does not exist");
+        }
+
+        // check the user's permission level
+        Integer userID = userService.getId();
+        Permission myPermission = permissionService.findPermission(departmentAddTo, userID);
+        if (myPermission != null && myPermission.getAuthorityLevel().getLevel() < PermissionLevel.DELETE.getLevel()) {
+            return ResponseResult.fail("Do not have authority to delete new contact");
+        }
+
+        if (contactService.removeById(newContact)) {
+            return ResponseResult.suc("delete a new contact success");
+        }
+        return ResponseResult.fail("delete a new contact fail");
     }
 
     /**
@@ -65,6 +194,7 @@ public class ContactController {
         }
         return ResponseResult.suc("success",contacts);
     }
+
 
 
 }
--- src/main/java/tech/crm/crmserver/service/impl/ContactServiceImpl.java
+++ src/main/java/tech/crm/crmserver/service/impl/ContactServiceImpl.java
@@ -1,15 +1,18 @@
 package tech.crm.crmserver.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import tech.crm.crmserver.common.enums.PermissionLevel;
+import tech.crm.crmserver.common.utils.NullAwareBeanUtilsBean;
 import tech.crm.crmserver.dao.Contact;
+import tech.crm.crmserver.dao.Permission;
 import tech.crm.crmserver.dao.RecentContact;
+import tech.crm.crmserver.dto.ContactDTO;
 import tech.crm.crmserver.mapper.ContactMapper;
-import tech.crm.crmserver.service.AttendService;
-import tech.crm.crmserver.service.ContactService;
+import tech.crm.crmserver.service.*;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
-import tech.crm.crmserver.service.RecentContactService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +33,15 @@ public class ContactServiceImpl extends ServiceImpl<ContactMapper, Contact> impl
 
     @Autowired
     private RecentContactService recentContactService;
+
+    @Autowired
+    public ContactMapper contactMapper;
+
+    @Autowired
+    public PermissionService permissionService;
+
+    @Autowired
+    public DepartmentService departmentService;
 
     /**
      * delete the Contact by departmentId<br/>
@@ -81,5 +93,92 @@ public class ContactServiceImpl extends ServiceImpl<ContactMapper, Contact> impl
         }
         attendService.deleteAttendByContactIds(contactIdList);
         recentContactService.deleteRecentContactByContactIds(contactIdList);
+    }
+
+    @Override
+    public Page<Contact> getContactByOrgIdAndDepartmentId(Page<Contact> page, Integer organizationId, Integer departmentId, Integer userId) {
+
+        QueryWrapper<Contact> queryWrapper = new QueryWrapper<>();
+        Page<Contact> contacts = new Page<>();
+        // User加入的departmentId
+        List<Permission> permissionList = permissionService.getPermissionByUserId(userId, PermissionLevel.DISPLAY);
+        List<Integer> departmentIdJoinList = new ArrayList<>();
+        for (Permission permission : permissionList) {
+            departmentIdJoinList.add(permission.getDepartmentId());
+        }
+
+        if (departmentId == null) {
+
+            // Org的departmentId
+            List<Integer> departmentIdList = departmentService.getDepartmentIdByOrganization(organizationId);
+
+            List<Integer> departmentInOrgAndJoin = new ArrayList<>();
+            for (Integer id : departmentIdList) {
+                if (departmentIdJoinList.contains(id)) {
+                    departmentInOrgAndJoin.add(id);
+                }
+            }
+
+            for (Integer department : departmentInOrgAndJoin) {
+                queryWrapper.or().eq("department_id", department);
+            }
+            page = contactMapper.selectPage(page, queryWrapper);
+        } else {
+            if (departmentIdJoinList.contains(departmentId)) {
+                queryWrapper.eq("department_id", departmentId);
+                page = contactMapper.selectPage(page, queryWrapper);
+            }
+        }
+
+        return page;
+    }
+
+    @Override
+    public Contact fromContactDTO(ContactDTO contactDTO) {
+        Contact contact = new Contact();
+        NullAwareBeanUtilsBean.copyProperties(contactDTO, contact);
+        return contact;
+    }
+
+    @Override
+    public List<Contact> getContactBasedOnSomeConditionFromDB(Integer departmentId, String email, String firstName, String middleName, String lastName, String phone, String gender, String customerType, String status) {
+        QueryWrapper<Contact> queryWrapper = new QueryWrapper<>();
+        if (departmentId != null) {
+            queryWrapper.eq("department_id", departmentId);
+        }
+        if (email != null) {
+            queryWrapper.eq("email", email);
+        }
+        if (firstName != null) {
+            queryWrapper.eq("first_name", firstName);
+        }
+        if (middleName != null) {
+            queryWrapper.eq("middle_name", middleName);
+        }
+        if (lastName != null) {
+            queryWrapper.eq("last_name", lastName);
+        }
+        if (phone != null) {
+            queryWrapper.eq("phone", phone);
+        }
+        if (gender != null) {
+            queryWrapper.eq("gender", gender);
+        }
+        if (customerType != null) {
+            queryWrapper.eq("customer_type", customerType);
+        }
+        if (status != null) {
+            queryWrapper.eq("status", status);
+        }
+        List<Contact> contacts = contactMapper.selectList(queryWrapper);
+        return contacts;
+    }
+
+    @Override
+    public boolean updateContact(Contact contact) {
+        if (contactMapper.updateById(contact)> 0) {
+            return true;
+        };
+        return false;
     }
 }
