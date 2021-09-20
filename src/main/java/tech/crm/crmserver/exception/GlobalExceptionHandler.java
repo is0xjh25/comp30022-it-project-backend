c15c3a8e0c5d04b8c6d6f19ce5f9dde43d352cdc
Lingxiao108105 <71265122+Lingxiao108105@users.noreply.github.com>
2021-09-20 22:01:11 +1000

    update Response of UNAUTHORIZED

--- src/main/java/tech/crm/crmserver/exception/GlobalExceptionHandler.java
+++ src/main/java/tech/crm/crmserver/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package tech.crm.crmserver.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
