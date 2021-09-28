6d3cb1af85fbbd9c872e0db7f29e88f5f4d15d28
Lingxiao108105 <71265122+Lingxiao108105@users.noreply.github.com>
2021-09-28 21:01:05 +1000

    Add: Implement Verify user after register through email && Update register, login and reset Password to fix verify user function

---
+++ src/main/java/tech/crm/crmserver/exception/UserNotActiveException.java
@@ -0,0 +1,25 @@
+package tech.crm.crmserver.exception;
+
+import org.springframework.http.HttpStatus;
+import tech.crm.crmserver.common.constants.ExceptionMessageConstants;
+import tech.crm.crmserver.common.response.ResponseResult;
+
+/**
+ * <p>
+ *  The Exception When The user is not active(is pending)
+ * </p>
+ *
+ * @author Lingxiao
+ * @since 2021-09-28
+ */
+public class UserNotActiveException extends BaseException{
+
+    public UserNotActiveException() {
+        super(ResponseResult.fail(ExceptionMessageConstants.USER_NOT_ACTIVE_EXCEPTION));
+    }
+
+    public UserNotActiveException(HttpStatus httpStatus) {
+        super(ResponseResult.fail(ExceptionMessageConstants.USER_NOT_ACTIVE_EXCEPTION, httpStatus));
+    }
+
+}
