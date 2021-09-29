1b5c22e2387334311d698f6f9b9bc7a2844250c5
kzzhe2333 <kzzhe@student.unimelb.edu.au>
2021-09-29 22:40:35 +0800

    update the exception comments

--- src/main/java/tech/crm/crmserver/exception/ToDoListInactiveException.java
+++ src/main/java/tech/crm/crmserver/exception/ToDoListInactiveException.java
@@ -3,6 +3,14 @@ package tech.crm.crmserver.exception;
 import tech.crm.crmserver.common.constants.ExceptionMessageConstants;
 import tech.crm.crmserver.common.response.ResponseResult;
 
+/**
+ * <p>
+ *  The Exception When the update target todoList is inactive
+ * </p>
+ *
+ * @author Kaiyuan
+ * @since 2021-09-29
+ */
 public class ToDoListInactiveException extends BaseException {
     public ToDoListInactiveException() {
         super(ResponseResult.fail(ExceptionMessageConstants.TODOLIST_INACTIVE_EXCEPTION));
--- src/main/java/tech/crm/crmserver/exception/TodoListUpdateFailException.java
+++ src/main/java/tech/crm/crmserver/exception/TodoListUpdateFailException.java
@@ -3,6 +3,14 @@ package tech.crm.crmserver.exception;
 import tech.crm.crmserver.common.constants.ExceptionMessageConstants;
 import tech.crm.crmserver.common.response.ResponseResult;
 
+/**
+ * <p>
+ *  The Exception When can not update todoList to database
+ * </p>
+ *
+ * @author Kaiyuan
+ * @since 2021-09-29
+ */
 public class TodoListUpdateFailException extends BaseException {
     public TodoListUpdateFailException() {
         super(ResponseResult.fail(ExceptionMessageConstants.TODOLIST_FAIL_ADDED_EXCEPTION));
